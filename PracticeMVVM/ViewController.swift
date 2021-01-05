16b19dcea4d1c81090a7d5a15c47d9224b53c44a
Kawa AKimune <akimune1003@gmail.com>
2021-01-05 20:22:34 +0900

    Initial Commit

---
+++ PracticeMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeMVVM
+//
+//  Created by 河明宗 on 2021/01/05.
+//  Copyright © 2021 河明宗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
